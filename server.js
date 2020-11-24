547f9e49767da3858f976bb6f2db35450865dd0c
Keith McKenna <keithmckenna@gmail.com>
2020-11-24 22:31:54 +0000

    create server file

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'CatChat Bot 🐱'
+
+// Run when client connects
+io.on('connection', socket => {
+	// Welcome current user
+	socket.emit('message', formatMessage(botName, 'Welcome to CatChat!'));
+	
+	// Broadcast when a user connects
+	socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+	
+	// Broadcast when client/user disco
+	socket.on('disconnect', () => {
+		io.emit('message', formatMessage(botName, 'A user has left the chat'));
+	});
+	
+	// Listen for chatMessage
+	socket.on('chatMessage', msg => {
+		io.emit('message', formatMessage('USER', msg));
+	});
+});
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, process.env.IP, () => console.log(`Server running on port ${PORT}`));
