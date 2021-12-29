9316567bc1154bba3c97dee6ce3f42091cf109de
Keith McKenna <keithmckenna@gmail.com>
2021-12-28 20:11:31 -0600

    prettier formatting ran and section added listening for user typing socket to emit typing with user data or return false

--- server.js
+++ server.js
@@ -1,64 +1,86 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'CatChat B0T 😺'
+const botName = "@mB0T 🤖";
 
 // Run when client connects
-io.on('connection', socket => {
-	socket.on('joinRoom', ({ username, room }) => {
-		const user = userJoin(socket.id, username, room);
-		
-		socket.join(user.room);
-		
-	// Welcome current user
-	socket.emit('message', formatMessage(botName, 'Welcome to CatChat!'));
-	
-	// Broadcast when a user connects
-	socket.broadcast
-		.to(user.room)
-		.emit('message',
-			  formatMessage(botName, `${user.username} has joined the chat`));
-		
-		// Send users and room info
-		io.to(user.room).emit('roomUsers', {
-			room: user.room,
-			users: getRoomUsers(user.room)
-		});
-	});
-	
-	// Listen for chatMessage
-	socket.on('chatMessage', msg => {
-		const user = getCurrentUser(socket.id);
-		io.to(user.room).emit('message', formatMessage(user.username, msg));
-	});
-	
-	// Broadcast when client/user disco
-	socket.on('disconnect', () => {
-		const user = userLeave(socket.id);
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-		if(user) {
-		io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
-			
-	// Send users and room info
-		io.to(user.room).emit('roomUsers', {
-			room: user.room,
-			users: getRoomUsers(user.room)
-		});
-		}
-	});
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to mChat!"));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for user typing
+  socket.on("is typing", (data) => {
+    const user = getCurrentUser(socket.id);
+    if (data === false) {
+      socket.to(user.room).emit("typing", false);
+    } else {
+      socket.to(user.room).emit("typing", formatMessage(user.username));
+    }
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Broadcast when client/user disco
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, process.env.IP, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, process.env.IP, () =>
+  console.log(`Server running on port ${PORT}`)
+);
